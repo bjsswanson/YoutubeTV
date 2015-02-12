a96b9f3b396ef367adbc7f9cfd913695efd06df5
swansonb <bjsswanson@gmail.com>
2015-02-12 09:53:03 +0000

    Updating6

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ var app = express();
 
 var port = 3000;
 
+var YoutubeTV = require('./yttv-server');
+YoutubeTV.IO = require('socket.io').listen(app.listen(port));
+YoutubeTV.OMX = require('./omxcontrol');
+YoutubeTV.Sockets.init();
+
 var expressHbs = require('express-handlebars');
 
 app.engine('hbs', expressHbs({extname:'hbs', defaultLayout:'main.hbs'}));
@@ -10,12 +15,8 @@ app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res){
-  res.render('index');
+  res.render('index', { 'playing' : YoutubeTV.Playing });
 });
 
 console.log('Listening on port ' + port);
 
-var YoutubeTV = require('./yttv-server');
-YoutubeTV.IO = require('socket.io').listen(app.listen(port));
-YoutubeTV.OMX = require('./omxcontrol');
-YoutubeTV.Sockets.init();
