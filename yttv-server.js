036b78cd377a18a2855a596124d0120918f03977
swansonb <bjsswanson@gmail.com>
2015-02-20 14:32:51 +0000

    Stopping for weekend

--- yttv-server.js
+++ yttv-server.js
@@ -1,3 +1,5 @@
+var startDay = 1;
+var endDay = 5
 var startHour = 8;
 var startMinute = 0;
 var endHour = 16;
@@ -238,16 +240,21 @@ YoutubeTV.Video = function(){
 
 	function isPastStopTime(){
 		var date = new Date();
+		var current_day = date.getDay();
 		var current_hour = date.getHours();
 		var current_minute = date.getMinutes();
-		return (current_hour <= startHour && current_minute <= startMinute) || (current_hour >= endHour && current_minute >= endMinute);
+		var workday = current_day >= startDay && current_day <= endDay;
+		var before_time = current_hour <= startHour && current_minute <= startMinute;
+		var after_time = current_hour >= endHour && current_minute >= endMinute;
+		return workday && (before_time || after_time) ;
 	}
 
 
 	function millisToStart(){
 		var start = new Date();
 		var end = new Date();
-		end.setDate(end.getDate() + 1);
+		var nextDay = start.getDay() >= endDay ? 3 : 1;
+		end.setDate(end.getDate() + nextDay);
 		end.setHours(startHour);
 		end.setMinutes(startMinute);
 		return end.getTime() - start.getTime();
