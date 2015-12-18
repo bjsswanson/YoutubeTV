9fe5f44619de96002529583c1e3d754a41044652
swansonb <LionLogin1!>
2015-12-18 08:27:17 +0000

    Duration doesnt work for playlist items

--- yttv-server.js
+++ yttv-server.js
@@ -216,7 +216,7 @@ YoutubeTV.Video = function(){
 		if(playlistId != undefined){
 			YoutubeTV.Youtube.playlistItems.list({
 				playlistId: playlistId,
-				part: 'snippet,contentDetails',
+				part: 'snippet',
 				maxResults:  50
 			}, function(err, data, res){
 				if(data != undefined && data.items.length > 0){
@@ -228,7 +228,6 @@ YoutubeTV.Video = function(){
 							id: item.snippet.resourceId.videoId,
 							title: item.snippet.title,
 							image: item.snippet.thumbnails != undefined ? item.snippet.thumbnails.default : { url : ""},
-                            duration: convertDuration(item.contentDetails.duration)
 						})
 					});
 
@@ -318,39 +317,43 @@ YoutubeTV.Video = function(){
 	}
 
     function convertDuration(duration) {
-        var a = duration.match(/\d+/g);
+        if(duration) {
+            var a = duration.match(/\d+/g);
 
-        if (duration.indexOf('M') >= 0 && duration.indexOf('H') == -1 && duration.indexOf('S') == -1) {
-            a = [0, a[0], 0];
-        }
+            if (duration.indexOf('M') >= 0 && duration.indexOf('H') == -1 && duration.indexOf('S') == -1) {
+                a = [0, a[0], 0];
+            }
 
-        if (duration.indexOf('H') >= 0 && duration.indexOf('M') == -1) {
-            a = [a[0], 0, a[1]];
-        }
-        if (duration.indexOf('H') >= 0 && duration.indexOf('M') == -1 && duration.indexOf('S') == -1) {
-            a = [a[0], 0, 0];
-        }
+            if (duration.indexOf('H') >= 0 && duration.indexOf('M') == -1) {
+                a = [a[0], 0, a[1]];
+            }
+            if (duration.indexOf('H') >= 0 && duration.indexOf('M') == -1 && duration.indexOf('S') == -1) {
+                a = [a[0], 0, 0];
+            }
 
-        duration = 0;
+            duration = 0;
 
-        if (a.length == 3) {
-            duration = duration + parseInt(a[0]) * 3600;
-            duration = duration + parseInt(a[1]) * 60;
-            duration = duration + parseInt(a[2]);
-        }
+            if (a.length == 3) {
+                duration = duration + parseInt(a[0]) * 3600;
+                duration = duration + parseInt(a[1]) * 60;
+                duration = duration + parseInt(a[2]);
+            }
 
-        if (a.length == 2) {
-            duration = duration + parseInt(a[0]) * 60;
-            duration = duration + parseInt(a[1]);
-        }
+            if (a.length == 2) {
+                duration = duration + parseInt(a[0]) * 60;
+                duration = duration + parseInt(a[1]);
+            }
 
-        if (a.length == 1) {
-            duration = duration + parseInt(a[0]);
+            if (a.length == 1) {
+                duration = duration + parseInt(a[0]);
+            }
+            var h = Math.floor(duration / 3600);
+            var m = Math.floor(duration % 3600 / 60);
+            var s = Math.floor(duration % 3600 % 60);
+            return ((h > 0 ? h + ":" + (m < 10 ? "0" : "") : "") + m + ":" + (s < 10 ? "0" : "") + s);
+        } else {
+            return "Unknown";
         }
-        var h = Math.floor(duration / 3600);
-        var m = Math.floor(duration % 3600 / 60);
-        var s = Math.floor(duration % 3600 % 60);
-        return ((h > 0 ? h + ":" + (m < 10 ? "0" : "") : "") + m + ":" + (s < 10 ? "0" : "") + s);
     }
 
 	var expose = {
