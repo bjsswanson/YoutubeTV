b516420a5fe1c09b8e7db5589097315fbcf0e75f
swansonb <ben.swanson@digitaslbi.com>
2016-03-01 14:46:36 +0000

    in Progress: iPlayer file download and play

--- omxcontrol.js
+++ omxcontrol.js
@@ -80,8 +80,8 @@ var OMX = function(){
 	function downloadIPlayer(url, id){
 		findIPlayerFile(id, function(iPlayerFile){
 			if(!iPlayerFile){
-				child_process.spawn("get_iplayer", [url, "--output", "/media/pi/MOVIES/IPLAYER"]);
-				child_process.spawn("get_iplayer", [url, "--subtitles-only", "--output", "/media/pi/MOVIES/IPLAYER"]);
+				child_process.spawn("get_iplayer", [url, "--output", "/media/pi/MOVIES/IPLAYER"], { stdio: 'inherit' });
+				child_process.spawn("get_iplayer", [url, "--subtitles-only", "--output", "/media/pi/MOVIES/IPLAYER"], { stdio: 'inherit' });
 			}
 		})
 	}
